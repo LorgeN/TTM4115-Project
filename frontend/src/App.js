bd610f058f20a8cfb467079f38ca614ecd042424
oysteinhansen <oysteinhansen2500@gmail.com>
2023-04-17 15:38:30 +0200

    Revert "Tekstfil for oversikt over frontend"
    This reverts commit cd60d533a2ed48ce23ce65b7c6b4f7fa91d646ab.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container';
+
 function App() {
   return (
-    <Container>
     <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-  </Container>
-    
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
