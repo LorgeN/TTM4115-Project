3432883ebdda6f50f52e30dd031c0378b2d1ca2c
Victoria Hopso <victoria.hopso@gmail.com>
2023-04-17 16:37:14 +0200

    adds routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,19 @@ import * as React from 'react'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 import { TakeRat } from './pages/TakeRat';
+import { StudentHome } from './pages/StudentHome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-    <ChakraProvider>
-      <TakeRat />
+    <ChakraProvider >
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StudentHome />}/>
+        <Route path="takerat" element={<TakeRat />} />
+      </Routes>
+    </BrowserRouter>
     </ChakraProvider>
   )
 }
