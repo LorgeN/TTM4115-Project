7505c3a798fac78c6ef74114d888f30b229a462d
oysteinhansen2500 <oysteinhansen2500@gmail.com>
2023-04-26 00:31:21 +0200

    Made dynamic IRAT page, and MQTT publish on submit

--- frontend/src/components/RatCard.jsx
+++ frontend/src/components/RatCard.jsx
@@ -1,80 +1,93 @@
 import {
-    Radio,
-    Card,
-    CardHeader,
-    Heading,
-    Stack,
-    CardBody,
-    Box,
-    Text,
-    StackDivider,
-    CardFooter,
-    Button,
-    HStack
-  } from "@chakra-ui/react";
-  import { useNavigate } from "react-router-dom";
+  Radio,
+  RadioGroup,
+  Card,
+  CardHeader,
+  Heading,
+  Stack,
+  CardBody,
+  Box,
+  Text,
+  StackDivider,
+  CardFooter,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
+import { useState } from "react";
 
-  
-  export const RatCard = (props) => {
-  
-    // TODO: replace dummydata 
-      let name = "RAT 7: sequence diagrams"
-      let question = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nam."
-      const navigate = useNavigate();
+export const RatCard = (props) => {
+  const { register, handleSubmit, errors, reset, control } = useForm();
 
 
+  // TODO: replace dummydata
+  let name = "RAT 7: sequence diagrams";
+  let question =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nam.";
+  const navigate = useNavigate();
 
-      const submit = () => {
-        // TODO: save answers
-        navigate("/waitingroom");
-      }
-
-      const quit = () => {
-        // TODO: save progredd
-        navigate("/studenthome");
-
-      }
-  
-    return (
-      <Card>
-        <CardHeader>
-          <Heading size="md">{name}</Heading>
-          <Text >{props.ratType} RAT</Text>
-        </CardHeader>
-  
-        <CardBody>
-          <Stack
-            divider={<StackDivider />}
-            spacing="4"
-          >
-              {/* Questions, for each:  */}
-            <Box>
-              <Text
-                pt="2"
-                fontSize="sm"
-              >
-                {question}
-              </Text>
-  
-              {/* Answers: for alternative in question: radiobutton + text  */}
-  
-              <Stack py={2}>
-                <Radio value="1">Radio 1</Radio>
-                <Radio value="2">Radio 2</Radio>
-                <Radio value="3">Radio 3</Radio>
-                <Radio value="3">Radio 3</Radio>
-              </Stack>
-            </Box>
-          </Stack>
-        </CardBody>
-        <CardFooter >
-            <HStack >
-        <Button onClick={submit} colorScheme='blue'>Submit</Button>
-        <Button onClick={quit} colorScheme='orange'>Quit</Button>
-
-            </HStack>
-        </CardFooter>
-      </Card>
-    );
+  const onSubmit = (data) => {
+    // TODO: save answers
+    
+    props.setData(data)
+    
+    console.log(data); //DATA FRA RAT
+    
   };
-  
+
+  const quit = () => {
+    // TODO: save progredd
+    navigate("/studenthome");
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">{name}</Heading>
+        <Text>{props.ratType} RAT</Text>
+      </CardHeader>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {Object.keys(props.sporsmaal).map((key, i) => {
+          console.log(key);
+          return (
+            <Card>
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing="4">
+                  {/* Questions, for each:  */}
+                  <Box>
+                    <Text>{props.sporsmaal[key].beskrivelse}</Text>
+
+                    {/* Answers: for alternative in question: radiobutton + text  */}
+
+                    <RadioGroup>
+                      <Stack py={2}>
+                        {props.sporsmaal[key].alternativer.map((alt, i) => (
+                          <Radio
+                            {...register(key)}
+                            value={i.toString()}
+                            key={i}
+                          >
+                            {alt.alt}
+                          </Radio>
+                        ))}
+                      </Stack>
+                    </RadioGroup>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+          );
+        })}
+
+        <Button type="submit" colorScheme="blue">
+          Test
+        </Button>
+        <Button onClick={quit} colorScheme="orange">
+            Quit
+          </Button>
+      </form>
+
+    </Card>
+  );
+};
--- frontend/src/pages/TakeRat.jsx
+++ frontend/src/pages/TakeRat.jsx
@@ -1,67 +1,121 @@
-
 import { Container, Button } from "@chakra-ui/react";
 import { RatCard } from "../components/RatCard";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import mqtt from "precompiled-mqtt";
 
-
-
 export const TakeRat = () => {
-
-
-
+  const navigate = useNavigate()
+  const sporsmaal = { // dette er hardkodet JSON, kunne like gjerne vært fra mqtt
+    spm1: {
+      beskrivelse: "Dette er et test-spørsmål",
+      alternativer: [
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: true,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+      ],
+    },
+    spm2: {
+      beskrivelse: "Dette er et test-spørsmål",
+      alternativer: [
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: true,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+        {
+          alt: "Dette er et test-alternativ",
+          korrekt: false,
+        },
+      ],
+    },
+  }; // dette er hardkodet JSON, kunne like gjerne vært fra mqtt
 
   const [client, setClient] = useState(null);
   /*const [isSubed, setIsSub] = useState(false);*/
   const [payload, setPayload] = useState({});
   const [connectStatus, setConnectStatus] = useState("Connect");
+  const [data, setData] = useState({}); //data fra RAT
 
   const connect = () => {
-
     //const url = `ws://broker.emqx.io:8083/mqtt`;
     const options = {
       keepalive: 30,
-      protocolId: 'MQTT',
+      protocolId: "MQTT",
       protocolVersion: 4,
       clean: true,
       reconnectPeriod: 1000,
       connectTimeout: 30 * 1000,
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     };
-    
+
     //options.username = localStorage.getItem("User");
     options.username = "komsys";
     options.password = "komsys123";
-   console.log(options)
+    console.log(options);
 
-  
     setClient(mqtt.connect("ws://broker.emqx.io:8083/mqtt")); //#DENNE FUNKER I DET MINSTE
     //setClient(mqtt.connect("ws://mqtt-broker.tandberg.org:9001"), options) //# DENNE FUNKER IKKE (muligens noe funky med connection)
+  };
 
-  }
 
-  const publish = () => {
-    client.publish("takerat", 'hei', 1, error => {
-      if (error) {
-        console.log('Publish error: ', error);
+
+
+    const publish = () => {
+      if (!client) {
+        console.log("no client");
+        return;
       }
-    
-    });
+      console.log("publishing");
 
-  }
-  
+      client.publish("takerat", JSON.stringify(data), 1, (error) => {
+        if (error) {
+          console.log("Publish error: ", error);
+        } else {
+          navigate("/waitingroom")
+        }
+      });
+      client.publish("takerat", "yes", 1, (error) => {
+        if (error) {
+          console.log("Publish error: ", error);
+        }
+      });
+    };
+
+
+
   useEffect(() => {
-    const connection = new Promise((resolve, reject) => {
-      connect();
-      resolve();
-      reject();
-    }).then(
+    console.log(JSON.stringify(data));
+    publish();
+  }, [data]);
+
+  useEffect(() => {
       if (client) {
       console.log(client)
       console.log("connected")
       client.on("connect", () => {
         console.log("connected")
-        publish();
+        //publish();
         setConnectStatus("Connected");
         
         
@@ -69,7 +123,7 @@ export const TakeRat = () => {
       client.on("message", (topic, message) => {
         setPayload({ topic, message: message.toString() });
       });
-    });
+    };
     connect();
     
     console.log(connectStatus)
@@ -82,10 +136,12 @@ export const TakeRat = () => {
   },[]);
   return (
     <Container marginTop={10}>
-        
-        <Button onClick={publish} colorScheme={"blue"}>test</Button>
-        <RatCard ratType={"Individual"}></RatCard>
-        
+
+      <RatCard
+        ratType={"Individual"}
+        sporsmaal={sporsmaal}
+        setData={setData}
+      ></RatCard>
     </Container>
   );
 };
