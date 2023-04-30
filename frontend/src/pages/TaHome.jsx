24cac0411f2c445a19e6aefa40ff9bbd12f3fbb2
Victoria Hopso <victoria.hopso@gmail.com>
2023-04-30 15:36:15 +0200

    adds modal for adding a rat

--- frontend/src/pages/TaHome.jsx
+++ frontend/src/pages/TaHome.jsx
@@ -3,10 +3,21 @@ import {
   CardHeader,
   CardBody,
   Container,
+  CardFooter,
+  useDisclosure,
+  Button,
+  Center
 } from "@chakra-ui/react";
 import { AssistanceQ } from "../components/AssistanceQ";
+import { AddRatModal } from "../components/AddRatModal";
 
 export const TaHome = () => {
+
+  const {
+    isOpen: isAddOpen,
+    onOpen: onAddOpen,
+    onClose: onAddClose,
+  } = useDisclosure();
  
   return (
     <Container
@@ -20,6 +31,24 @@ export const TaHome = () => {
         <CardBody paddingBottom={20}>
           <AssistanceQ></AssistanceQ>
         </CardBody>
+        <Center paddingBottom={10}>
+           <Button
+                rounded={"full"}
+                px={6}
+                colorScheme={"orange"}
+                bg={"orange.400"}
+                _hover={{ bg: "orange.500" }}
+                onClick={() => onAddOpen()}
+              >
+                Create new RAT
+              </Button>
+          <AddRatModal 
+          isOpen={isAddOpen}
+          onOpen={onAddOpen}
+          onClose={onAddClose}>
+
+          </AddRatModal>
+        </Center>
       </Card>
     </Container>
   );
