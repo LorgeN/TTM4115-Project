da6528f086191fa0c53eff1535d2ce4f19712ce8
oysteinhansen <oysteinhansen2500@gmail.com>
2023-05-01 11:12:42 +0200

    waitingroom kan nå knyttes til MQTT. TRat låses opp ved riktig beskjed fra riktig topic

--- frontend/src/pages/WaitingRoom.jsx
+++ frontend/src/pages/WaitingRoom.jsx
@@ -1,11 +1,51 @@
 import { Button, Card, Container, Heading, Stack } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-
+import mqtt from "precompiled-mqtt";
 
 export const WaitingRoom = () => {
   const [isReady, setIsReady] = useState(false);
   const navigate = useNavigate()
+  const [client, setClient] = useState(null);
+  const [payload, setPayload] = useState({});
+  const [connectStatus, setConnectStatus] = useState("Connect");
+  const [isSubed, setIsSub] = useState(false);
+
+/* useEffect for å sette opp connection */
+  useEffect(() => {
+    if (!client){
+      setClient(mqtt.connect("ws://broker.emqx.io:8083/mqtt"));
+    }
+    if (client) {
+      client.on('connect', () => {
+        
+        client.subscribe("takerattma4115", "0", (error) => {
+          if (error) {
+            console.log('Subscribe to topics error', error)
+            return
+          }
+          setIsSub(true)
+        });
+        setConnectStatus('Connected');
+      });
+      client.on('error', (err) => {
+        console.error('Connection error: ', err);
+        client.end();
+      });
+      client.on('reconnect', () => {
+        setConnectStatus('Reconnecting');
+      });
+      client.on('message', (topic, message) => {
+        if (topic === 'takerattma4115') {
+          if (JSON.parse(message.toString())["msg"] === 'true') { // Hvis topic er "takerattma4115" og meldingen er "msg: true", så er TRAT ready
+            setIsReady(true);
+          }
+        }
+        const payload = { topic, message: message.toString() };
+        setPayload(payload);
+      });
+    }
+  }, [client]); /*  connection useEffect */
 
   const startTRat = () => {
     navigate("/takeTRat")
@@ -19,7 +59,7 @@ export const WaitingRoom = () => {
 
       <Heading>Waiting to start Team RAT </Heading>
       <Button 
-          // isDisabled={!isReady}
+          isDisabled={!isReady}
           colorScheme={"green"}
           onClick={startTRat}
 
